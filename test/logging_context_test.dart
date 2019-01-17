9f87b8c7cca4d7424949e0551a697d5f224caf45
Roman Vanesyan <romain.vanesyan@gmail.com>
2019-01-17 16:46:59 +0500

    Remove unnecessary tests.

--- test/logging_context_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:test/test.dart';
-import 'package:structlog/structlog.dart';
-import 'package:structlog/src/internals/logging_context.dart';
-
-void main() {
-  group('LoggingContext', () {
-    test('#bind works correctly', () {});
-  });
-}
