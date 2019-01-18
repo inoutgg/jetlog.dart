2ffc4471b1e0ff55e17df42dbf16f63db48612b7
Roman Vanesyan <romain.vanesyan@gmail.com>
2019-01-18 16:03:15 +0500

    Clarify which fields may be null on `Record`.

--- lib/src/record.dart
+++ lib/src/record.dart
@@ -20,11 +20,15 @@ abstract class Record {
   String get message;
 
   /// Name of the logger this record originally emitted by.
+  ///
+  /// If the logger has no name, may be omitted.
   String get name;
 
   /// Time when this record was created.
   DateTime get time;
 
   /// A set of fields bound to the logging context.
+  ///
+  /// May be omitted if no fields are bound to the logging context.
   Iterable<Field> get fields;
 }
