c8f78d11cb512a56e78a4db3ff69fac82936292b
Roman Vanesyan <romain.vanesyan@gmail.com>
2019-04-05 23:56:13 +0500

    Tweak example.

--- example/stdout_stderr.dart
+++ example/stdout_stderr.dart
@@ -1,3 +1,4 @@
+import 'dart:async' show Future;
 import 'dart:io' show stdout, stderr;
 
 import 'package:structlog/structlog.dart'
@@ -5,8 +6,8 @@ import 'package:structlog/structlog.dart'
 import 'package:structlog/handlers.dart' show MultiHandler, StreamHandler;
 import 'package:structlog/formatters.dart' show TextFormatter;
 
-class StderrOnlyLevelFilter extends Filter {
-  StderrOnlyLevelFilter();
+class _StderrOnlyLevelFilter extends Filter {
+  _StderrOnlyLevelFilter();
 
   @override
   bool filter(Record record) =>
@@ -15,8 +16,8 @@ class StderrOnlyLevelFilter extends Filter {
       record.level == Level.fatal;
 }
 
-class StdoutOnlyLevelFilter extends Filter {
-  StdoutOnlyLevelFilter();
+class _StdoutOnlyLevelFilter extends Filter {
+  _StdoutOnlyLevelFilter();
 
   @override
   bool filter(Record record) =>
@@ -25,23 +26,23 @@ class StdoutOnlyLevelFilter extends Filter {
       record.level == Level.info;
 }
 
+final _formatter = TextFormatter(
+    format: ({name, level, time, message, fields}) =>
+        '$name $time [$level]: $message $fields');
+
+final _stderrHandler = StreamHandler(stderr.nonBlocking)
+  ..formatter = _formatter.asBytesFormatter
+  ..addFilter(_StderrOnlyLevelFilter());
+final _stdoutHandler = StreamHandler(stdout.nonBlocking)
+  ..formatter = _formatter.asBytesFormatter
+  ..addFilter(_StdoutOnlyLevelFilter());
+
+final _logger = Logger.getLogger('example.stdout')
+  ..level = Level.all
+  ..handler = MultiHandler([_stdoutHandler, _stderrHandler]);
+
 Future<void> main() async {
-  final formatter = TextFormatter(
-      format: ({name, level, time, message, fields}) =>
-          '$name $time [$level]: $message $fields');
-
-  final stderrHandler = StreamHandler(stderr.nonBlocking)
-    ..formatter = formatter.bytes
-    ..addFilter(StderrOnlyLevelFilter());
-  final stdoutHandler = StreamHandler(stdout.nonBlocking)
-    ..formatter = formatter.bytes
-    ..addFilter(StdoutOnlyLevelFilter());
-
-  final logger = Logger.getLogger('example.stdout')
-    ..level = Level.all
-    ..handler = MultiHandler([stdoutHandler, stderrHandler]);
-
-  final tracer = logger.bind([
+  final tracer = _logger.bind([
     const Str('username', 'vanesyan'),
     const Str('filename', 'avatar.png'),
     const Str('mime', 'image/png'),
