67bfeef0004d6c01438f16d5a41fb0f8c78cb517
Roman Vanesyan <romain.vanesyan@gmail.com>
2019-06-23 01:43:55 +0500

    Update `StreamHandler` now it accepts streams that works only with `List<int>` input.

--- lib/src/handlers/stream_handler.dart
+++ lib/src/handlers/stream_handler.dart
@@ -1,29 +1,32 @@
 import 'dart:async' show StreamSink;
-import 'package:structlog/structlog.dart' show FiltererBase, Handler, Record;
+
+import 'package:meta/meta.dart' show required;
+import 'package:structlog/structlog.dart' show Handler, Record, Filter;
 import 'package:structlog/formatters.dart' show Formatter;
 
 /// [StreamHandler] delegates log records downstream.
-class StreamHandler<T> extends Handler with FiltererBase {
-  StreamHandler(this._stream);
+///
+/// Each log record is formatted using the `formatter` before
+/// submitting it to the downstream.
+class StreamHandler extends Handler {
+  StreamHandler(this._stream, {@required Formatter formatter})
+      : _formatter = formatter;
 
-  final StreamSink<T> _stream;
-  Formatter<T> _formatter;
+  final Formatter _formatter;
+  final StreamSink<List<int>> _stream;
+  Filter _filter;
 
-  /// Sets records formatter.
-  set formatter(Formatter<T> formatter) {
-    ArgumentError.checkNotNull(formatter, 'formatter');
-
-    _formatter = formatter;
-  }
+  /// Sets records filterer.
+  ///
+  /// Set filterer behaves the same way as a [Logger] filter.
+  set filter(Filter filter) => _filter = filter;
 
   @override
   void handle(Record record) {
-    if (!filter(record)) {
+    if (_filter != null && !_filter.filter(record)) {
       return;
     }
 
-    final result = _formatter(record);
-
-    _stream.add(result);
+    _stream.add(_formatter(record));
   }
 }
