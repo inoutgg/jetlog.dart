e5c245b2faa15764908a9294a60606315a90682e
Roman Vanesyan <romain.vanesyan@gmail.com>
2019-07-02 14:12:16 +0500

    100% coverage `Field`s

--- test/field_test.dart
+++ test/field_test.dart
@@ -3,6 +3,30 @@ import 'package:jetlog/jetlog.dart';
 
 void main() {
   group('Field', () {
+    test('fields are equals when both have the same name', () {
+      final f1 = Field<dynamic>(name: 'name', value: 123, kind: FieldKind.any);
+      final f2 =
+          Field<dynamic>(name: 'name', value: 'str', kind: FieldKind.any);
+      final f3 =
+          Field<dynamic>(name: 'name', value: 'str', kind: FieldKind.any);
+      final f4 =
+          Field<dynamic>(name: 'other-name', value: 'str', kind: FieldKind.any);
+
+      // reflectivity
+      expect(f1, equals(f1));
+      expect(f2, equals(f2));
+
+      // symmetry
+      expect(f1, equals(f2));
+      expect(f2, equals(f1));
+
+      // transitivity
+      expect(f1, equals(f3));
+      expect(f2, equals(f3));
+
+      expect(f1, isNot(equals(f4)));
+    });
+
     group('Dur', () {
       test('Sets correct `FieldKind`', () {
         const field = Dur('Dur', Duration.zero);
