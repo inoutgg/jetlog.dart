e708a8b3cd65d053023cfeb7337f8453a98317ce
Roman Vanesyan <romain.vanesyan@gmail.com>
2019-07-02 15:12:24 +0500

    Add `stream_handler_test.dart` to `all.dart`

--- test/all.dart
+++ test/all.dart
@@ -6,6 +6,7 @@ import 'level_test.dart' as level_test;
 import 'logger_test.dart' as logger_test;
 import 'multi_filter_test.dart' as multi_filter_test;
 import 'multi_handler_test.dart' as multi_handler_test;
+import 'steam_handler_test.dart' as steam_handler_test;
 import 'text_formatter_test.dart' as text_formatter_test;
 import 'tracer_test.dart' as tracer_test;
 
@@ -27,4 +28,5 @@ void main() {
   // Handlers tests
   console_handler_test.main();
   multi_handler_test.main();
+  steam_handler_test.main();
 }
