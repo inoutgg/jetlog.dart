0744fa942be385909640bc83a86de356db00df9f
Roman Vanesyan <roman_vanesyan@epam.com>
2019-10-07 15:42:38 +0500

    (feat) Update Formatter class to typedef, so functions can also be used as formatters.

--- lib/src/formatters/formatter.dart
+++ lib/src/formatters/formatter.dart
@@ -5,8 +5,4 @@ typedef TimestampFormatter<T> = T Function(DateTime);
 typedef FieldsFormatter<F> = F Function(Iterable<Field>);
 
 /// [Formatter] is capable to format single [Record] entry.
-abstract class Formatter {
-  /// Formats given [record] and returns formatted string as
-  /// UTF-8 bytes sequence.
-  List<int> call(Record record);
-}
+typedef Formatter = List<int> Function(Record);
