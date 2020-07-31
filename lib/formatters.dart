b7b5a71489f252b4a815eb08cc14d325cc61a556
Roman Vanesyan <roman@vanesyan.com>
2020-08-01 01:54:50 +0500

    (feat) add testcases and docs

--- lib/formatters.dart
+++ lib/formatters.dart
@@ -1,6 +1,6 @@
 library jetlog.formatters;
 
 export 'src/formatters/formatter.dart'
-    show Formatter, TimestampFormatter, LevelFormatter, FieldsFormatter;
+    show Formatter, TimestampFormatter, LevelFormatter, FormatterBase;
 export 'src/formatters/json_formatter.dart' show JsonFormatter;
 export 'src/formatters/text_formatter.dart' show TextFormatter;
--- lib/src/formatters/formatter.dart
+++ lib/src/formatters/formatter.dart
@@ -7,14 +7,21 @@ typedef FieldFormatter<R> = R Function(Field<Object>);
 /// [Formatter] is capable to format single [Record] entry.
 typedef Formatter = List<int> Function(Record);
 
+/// Base mixin for implementing [Formatter].
 ///
+/// Contains default implementations of common methods across formatters.
 mixin FormatterBase<R> {
   final Map<FieldKind, FieldFormatter<R>> _registeredFieldFormatters = {};
 
+  /// Registers [formatter] for a given field [kind].
   void setFieldFormatter(FieldKind kind, FieldFormatter<R> formatter) {
     _registeredFieldFormatters[kind] = formatter;
   }
 
+  /// Returns registered formatter for a give field [kind].
+  ///
+  /// If no formatter is registered for given field [kind] throws an
+  /// appropriated [StateError].
   FieldFormatter<R> getFieldFormatter(FieldKind kind) {
     final formatter = _registeredFieldFormatters[kind];
     if (formatter == null) {
