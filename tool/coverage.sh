c7287768759ac99dd1efe52022bc21e454bd17d1
Roman Vanesyan <roman@vanesyan.com>
2020-09-09 12:19:53 +0500

    build: fix code cov

--- tool/coverage.sh
+++ tool/coverage.sh
@@ -8,14 +8,15 @@ pub global activate coverage &>/dev/null
 
 echo "Collecting coverage data on port $OBSERVATORY_PORT..."
 
-dart --disable-service-auth-codes \
+dart --enable-experiment=non-nullable \
+    --disable-service-auth-codes \
     --enable-vm-service=${OBSERVATORY_PORT} \
     --pause-isolates-on-exit \
     test/all.dart &
 
 pub global run coverage:collect_coverage \
     --port=${OBSERVATORY_PORT} \
-    --out=tmp/coverage.json \
+    --out=/tmp/coverage.json \
     --wait-paused \
     --resume-isolates
 
@@ -23,7 +24,7 @@ echo "Generating LCOV report..."
 
 pub global run coverage:format_coverage \
     --lcov \
-    --in=tmp/coverage.json \
-    --out=tmp/lcov.info \
+    --in=/tmp/coverage.json \
+    --out=/tmp/lcov.info \
     --packages=.packages \
     --report-on=lib
