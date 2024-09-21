0057bc98b0f4d367367c9f317b346279335b74a5
Roman Vanesyan <roman@vanesyan.com>
2024-09-21 21:34:40 +0300

    chore: ???

---
+++ example/macro_example.dart
@@ -0,0 +1,38 @@
+/// This example shows how to setup a logger that delegates logging records
+/// to the console (using builtin `print` function) preliminarily formatted
+/// with default [TextFormatter].
+library example.console;
+
+import 'package:jetlog/formatters.dart' show TextFormatter;
+import 'package:jetlog/handlers.dart' show ConsoleHandler;
+import 'package:jetlog/jetlog.dart' show DefaultLog, Level, Logger, Str, Obj;
+import 'package:jetlog/macros.dart' show Loggable, field;
+
+@Loggable()
+class File {
+  File(this.name, this.mime);
+
+  @field
+  final String name;
+
+  @field
+  final String mime;
+}
+
+final _logger = Logger.detached()
+  ..level = Level.all
+  ..handler = ConsoleHandler(formatter: TextFormatter.withDefaults());
+
+Future<void> main() async {
+  final file = File('avatar.png', 'image/png');
+  final context = _logger
+      .withFields({const Str('username', 'roman-vanesyan'), Obj('file', file)});
+
+  final tracer = context.startTimer('Uploading!', level: Level.info);
+
+  // Emulate uploading, wait for 1 sec.
+  await Future<void>.delayed(const Duration(seconds: 1));
+
+  tracer.stop('Aborting...');
+  context.fatal('Failed to upload!');
+}
