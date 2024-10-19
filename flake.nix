6c177f904d14f7adc6d5f5e89bdff4e44394bd6d
Roman Vanesyan <roman@vanesyan.com>
2024-10-19 20:03:38 +0300

    feat: use dart-overlay

--- flake.nix
+++ flake.nix
@@ -4,23 +4,30 @@
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    dart.url = "github:roman-vanesyan/dart-overlay";
   };
 
   outputs =
-    {
+    inputs@{
       nixpkgs,
       flake-utils,
       ...
     }:
-    flake-utils.lib.eachDefaultSystem (
+    let
+      systems = builtins.attrNames inputs.dart.packages;
+    in
+    flake-utils.lib.eachSystem systems (
       system:
       let
-        pkgs = import nixpkgs { inherit system; };
+        overlays = [
+          inputs.dart.overlays.default
+        ];
+        pkgs = import nixpkgs { inherit system overlays; };
       in
       {
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
-            dart
+            dartpkgs.default
           ];
         };
 
